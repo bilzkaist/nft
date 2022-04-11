b7fef1058f62ca1d040fda4a4ef773f51bf5da43
Bilal Dastagir <bilzkaist@gmail.com>
2022-04-12 02:26:00 +0900

    nftcard.herokuapp.com

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // app.js
+//https://nftcard.herokuapp.com/
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+}).listen(process.env.PORT || 1337, 'nftcard.herokuapp.com');
+console.log('Server running at https://nftcard.herokuapp.com/');
+//console.log('Server running at http://127.0.0.1:1337/');
